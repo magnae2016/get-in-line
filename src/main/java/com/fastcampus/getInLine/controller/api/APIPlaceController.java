468b27e56a097d6c0214bdc150cf30fefa8a1423
magnae2016 <magnae2016@gmail.com>
2022-09-05 21:00:47 +0900

    Add API Controllers

---
+++ src/main/java/com/fastcampus/getInLine/controller/api/APIPlaceController.java
@@ -0,0 +1,35 @@
+package com.fastcampus.getInLine.controller.api;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RequestMapping("/api")
+@RestController
+public class APIPlaceController {
+    @GetMapping("/places")
+    public List<String> getPlaces() {
+        return Arrays.asList("place1", "place2");
+    }
+
+    @PostMapping("/places")
+    public Boolean createPlace() {
+        return true;
+    }
+
+    @GetMapping("/places/{placeId}")
+    public String getPlace(@PathVariable Integer placeId) {
+        return "place " + placeId;
+    }
+
+    @PutMapping("/places/{placeId}")
+    public Boolean modifyPlace(@PathVariable Integer placeId) {
+        return true;
+    }
+
+    @DeleteMapping("/places/{placeId}")
+    public Boolean removePlace(@PathVariable Integer placeId) {
+        return true;
+     }
+}
